27a9c7bb4a50659bcead822bc8d2ed3680edb581
smile2game <2426827419@qq.com>
2024-12-06 15:16:35 +0800

    roadmap

--- examples/cogvideox/sample.py
+++ examples/cogvideox/sample.py
@@ -2,11 +2,14 @@ from videosys import CogVideoXConfig, VideoSysEngine
 import argparse
 import time 
 
+model_path = "/data2/lhj_data/ops/CogVideoX-2b"
+# model_path = "/home/pod/shared-nvme/CogVideoX-2b"
 def run_base():
     # models: "THUDM/CogVideoX-2b" or "THUDM/CogVideoX-5b"
     # change num_gpus for multi-gpu inference
     # config = CogVideoXConfig("THUDM/CogVideoX-2b", num_gpus=1)
-    config = CogVideoXConfig("/home/pod/shared-nvme/CogVideoX-2b", num_gpus=1)
+    # config = CogVideoXConfig("/home/pod/shared-nvme/CogVideoX-2b", num_gpus=1)
+    config = CogVideoXConfig(model_path="/data2/lhj_data/ops/CogVideoX-2b", num_gpus=1)
 
     engine = VideoSysEngine(config)
 
@@ -15,14 +18,14 @@ def run_base():
     # seed=-1 means random seed. >0 means fixed seed.
     seed = 10 #这里可能会影响视频生成效果
     start = time.time()
-    import debugpy
-    try:
-        # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
-        debugpy.listen(("localhost", 9501))
-        print("Waiting for debugger attach")
-        debugpy.wait_for_client()
-    except Exception as e:
-        pass
+    # import debugpy
+    # try:
+    #     # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
+    #     debugpy.listen(("localhost", 9501))
+    #     print("Waiting for debugger attach")
+    #     debugpy.wait_for_client()
+    # except Exception as e:
+    #     pass
     video = engine.generate(
         prompt=prompt,
         guidance_scale=6,
@@ -32,11 +35,11 @@ def run_base():
     ).video[0]
     end = time.time()
     print(f"CogVideoX-2b run_base generate video cost time:{end-start}")
-    engine.save_video(video, f"./outputs/{prompt}-{seed}.mp4")
+    engine.save_video(video, f"./outputs/{prompt}-{seed}-4.mp4")
 
 
 def run_pab():
-    config = CogVideoXConfig("/home/pod/shared-nvme/CogVideoX-2b", enable_pab=True,num_gpus=1)
+    config = CogVideoXConfig(model_path, enable_pab=True,num_gpus=1)
     engine = VideoSysEngine(config)
 
     prompt = "Sunset over the sea."
@@ -48,7 +51,7 @@ def run_pab():
 
 
 def run_low_mem():
-    config = CogVideoXConfig("/home/pod/shared-nvme/CogVideoX-2b", cpu_offload=True, vae_tiling=True)
+    config = CogVideoXConfig(model_path, cpu_offload=True, vae_tiling=True)
     engine = VideoSysEngine(config)
 
     prompt = "Sunset over the sea."
