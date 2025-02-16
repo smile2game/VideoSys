6d0cf58a773597a1bf72e65280a89a6f70fb7b03
smile2game <2426827419@qq.com>
2025-02-16 14:58:59 +0800

    para-tech

--- examples/inference/cogvideox/sample.py
+++ examples/inference/cogvideox/sample.py
@@ -2,22 +2,8 @@ from videosys import CogVideoXConfig, VideoSysEngine
 import argparse
 import time 
 
-model_path = "/data2/lhj_data/ops/CogVideoX-2b"
-# model_path = "/home/pod/shared-nvme/CogVideoX-2b"
+model_path = "/root/shared-nvme/CogVideoX-2b"
 def run_base():
-    # models: "THUDM/CogVideoX-2b" or "THUDM/CogVideoX-5b"
-    # change num_gpus for multi-gpu inference
-    # config = CogVideoXConfig("THUDM/CogVideoX-2b", num_gpus=1)
-    # config = CogVideoXConfig("/home/pod/shared-nvme/CogVideoX-2b", num_gpus=1)
-    config = CogVideoXConfig(model_path="/data2/lhj_data/ops/CogVideoX-2b", num_gpus=1)
-
-    engine = VideoSysEngine(config)
-
-    prompt = "Sunset over the sea."
-    # num frames should be <= 49. resolution is fixed to 720p.
-    # seed=-1 means random seed. >0 means fixed seed.
-    seed = 10 #这里可能会影响视频生成效果
-    start = time.time()
     import debugpy
     try:
         # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
@@ -26,6 +12,14 @@ def run_base():
         debugpy.wait_for_client()
     except Exception as e:
         pass
+    config = CogVideoXConfig(model_path=model_path, num_gpus=1)
+    engine = VideoSysEngine(config)
+
+    prompt = "Sunset over the sea."
+    # num frames should be <= 49. resolution is fixed to 720p.
+    # seed=-1 means random seed. >0 means fixed seed.
+    seed = 10 #这里可能会影响视频生成效果
+    start = time.time()
     video = engine.generate(
         prompt=prompt,
         guidance_scale=6,
